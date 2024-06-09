9d5131b78dba7748af6eb0615f2a6cbda0f07a7c
Winfrey Chisvo <WinfreyC@analyte.co.za>
2024-06-09 11:53:27 +0200

    Define and handle own messages

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
+using System.Buffers.Text;
+
+namespace WinTail
+{
+    class Messages
+    {
+        #region Neutral/system messages
+        /// <summary>
+        /// Marker class to continue processing.
+        /// </summary>
+        public class ContinueProcessing { }
+        #endregion
+
+
+        #region Success messages
+
+        /// <summary>
+        /// Base class for signalling that user input was valid.
+        /// </summary>
+        public class InputSuccess
+        {
+            public InputSuccess(string reason)
+            {
+                Reason = reason; 
+            }
+            public string Reason { get; private set; }
+        }
+        #endregion
+
+
+        #region Error messages
+
+        /// <summary>
+        /// Base class for signalling that user input is invalid
+        /// </summary>
+        public class InputError
+        {
+            public InputError(string reason)
+            {
+                Reason = reason; 
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        /// <summary>
+        /// User provided blank input
+        /// </summary>
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// User privided invalid input (currently, input w/ odd # chars)
+        /// </summary>
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason)
+            {
+
+            }
+        }
+
+        #endregion
+
+    }
+
+}
