787747e5e29b2a92c9bf7f84e1e4836a2ecb34b1
Winfrey Chisvo <WinfreyC@analyte.co.za>
2024-06-13 03:12:27 +0200

    Add missing GetAndValidateInput method to ConsoleReaderActor

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -25,13 +25,29 @@ namespace WinTail
                 DoPrintInstructions();
             }
 
+            GetAndValidateInput();
         }
 
+       
         #region Internal methods
 
-        private void DoPrintInstructions()
+        private static void DoPrintInstructions()
         {
             Console.WriteLine("Please provide the URI of a log file on disk.\n");
+        }
+
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (!string.IsNullOrEmpty(message) && String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                // if user typed ExitCommand, shut down the entire actor system (allows the process to exit)
+                Context.System.Terminate();
+                return;
+            }
+
+            // otherwise, just hand message off to validation actor (by telling its actor ref)
+            _validationActor.Tell(message);
         }
 
         #endregion
